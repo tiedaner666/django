884510dea15b79f00f51eae79019bf2787284562
tiedaner <tiedaner666@gmail.com>
2019-04-17 16:44:45 +0800

    count_update

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('count/', views.count),
+
 ]
 
--- wordcount/views.py
+++ wordcount/views.py
@@ -3,3 +3,25 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def count(request):
+    user_text = request.GET['text']
+    total_count = len(request.GET['text'])
+
+    word_dict = {}
+
+    for word in user_text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    sorted_dict = sorted(word_dict.items(), key=lambda w: w[1], reverse=True)
+
+    return render(request, 'count.html', {'count': total_count, 'text': user_text,
+                  'word_list': word_dict, 'sorted': sorted_dict})
+
+
+
+
+
